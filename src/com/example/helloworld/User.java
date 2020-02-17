d0e225393289c4f4d702f09d57e6c5e3b4a90b40
ewru <ewelina.ruchlewicz@gmail.com>
2020-02-17 13:17:36 +0100

    [ABC-123] Add User class

---
+++ src/com/example/helloworld/User.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+class User {
+    private String name;
+
+    User(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+}
